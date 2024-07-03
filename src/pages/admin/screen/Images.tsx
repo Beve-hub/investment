bfea43e71f9cfc4e80af4035d191e8bb82cfb29f
Believe <victorokeke89@gmail.com>
2024-07-04 00:23:15 +0100

    ask

--- src/pages/admin/screen/Images.tsx
+++ src/pages/admin/screen/Images.tsx
@@ -13,7 +13,7 @@ const Images = () => {
     return (
         <div >
             <p className='font-bold text-xl py-4'>Client Valid IDs</p>
-            <div className='grid md:grid-cols-3'>
+            <div className='grid md:grid-cols-6'>
                 {images.map(image => (
                     <div className='bg-[--layer-colo] p-4 h-[13rem] w-[10rem] rounded-lg'>
                     <div key={image.imageUrl} className='max-h-[5rem] max-w-[10rem] '>
